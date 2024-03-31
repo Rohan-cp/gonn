24e662edef90e1f680ef758ac74949894324a50e
Rohan-cp <rohancp9@icloud.com>
2024-03-31 01:43:32 -0400

    added some util to save and load nn

--- mlp.go
+++ mlp.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math"
+	"os"
 
 	"gonum.org/v1/gonum/mat"
 	"gonum.org/v1/gonum/stat/distuv"
@@ -60,6 +61,7 @@ func (net Network) Train(inputData []float64, targetData []float64) {
 		scale(net.learningRate,
 			dot(multiply(outputErrors, sigmoidPrime(finalOutputs)),
 				hiddenOutputs.T()))).(*mat.Dense)
+
 	net.hiddenWeights = add(net.hiddenWeights,
 		scale(net.learningRate,
 			dot(multiply(hiddenErrors, sigmoidPrime(hiddenOutputs)),
@@ -142,4 +144,35 @@ func addScalar(i float64, m mat.Matrix) mat.Matrix {
 	}
 	n := mat.NewDense(r, c, a)
 	return add(m, n)
+}
+
+// save a neural network to file
+func save(net Network) {
+	h, err := os.Create("data/hweights.model")
+	defer h.Close()
+	if err == nil {
+		net.hiddenWeights.MarshalBinaryTo(h)
+	}
+	o, err := os.Create("data/oweights.model")
+	defer o.Close()
+	if err == nil {
+		net.outputWeights.MarshalBinaryTo(o)
+	}
+}
+
+// load a neural network from file
+func load(net Network) {
+	h, err := os.Open("data/hweights.model")
+	defer h.Close()
+	if err == nil {
+		net.hiddenWeights.Reset()
+		net.hiddenWeights.UnmarshalBinaryFrom(h)
+	}
+	o, err := os.Open("data/oweights.model")
+	defer o.Close()
+	if err == nil {
+		net.outputWeights.Reset()
+		net.outputWeights.UnmarshalBinaryFrom(o)
+	}
+	return
 }
