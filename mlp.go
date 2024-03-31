79b323ed2f773c960fa37ee762814640b3dbdc51
Rohan-cp <rohancp9@icloud.com>
2024-03-30 21:47:41 -0400

    base train method added on network!

--- mlp.go
+++ mlp.go
@@ -42,6 +42,30 @@ func randomArray(size int, v float64) (data []float64) {
 	return
 }
 
+func (net Network) Train(inputData []float64, targetData []float64) {
+	// forward propogation
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := dot(net.hiddenWeights, inputs)
+	hiddenOutputs := apply(sigmoid, hiddenInputs)
+	finalInputs := dot(net.outputWeights, hiddenOutputs)
+	finalOutputs := apply(sigmoid, finalInputs)
+
+	// find errors
+	targets := mat.NewDense(len(targetData), 1, targetData)
+	outputErrors := subtract(targets, finalOutputs)
+	hiddenErrors := dot(net.outputWeights.T(), outputErrors)
+
+	// backpropagate
+	net.outputWeights = add(net.outputWeights,
+		scale(net.learningRate,
+			dot(multiply(outputErrors, sigmoidPrime(finalOutputs)),
+				hiddenOutputs.T()))).(*mat.Dense)
+	net.hiddenWeights = add(net.hiddenWeights,
+		scale(net.learningRate,
+			dot(multiply(hiddenErrors, sigmoidPrime(hiddenOutputs)),
+				inputs.T()))).(*mat.Dense)
+}
+
 func (net Network) Predict(inputData []float64) mat.Matrix {
 	// feedforward
 	inputs := mat.NewDense(len(inputData), 1, inputData)
@@ -56,8 +80,17 @@ func sigmoid(r, c int, z float64) float64 {
 	return 1.0 / (1 + math.Exp(-1*z))
 }
 
-// Helper functions of easier use of Gonum
+func sigmoidPrime(m mat.Matrix) mat.Matrix {
+	rows, _ := m.Dims()
+	o := make([]float64, rows)
+	for i := range o {
+		o[i] = 1
+	}
+	ones := mat.NewDense(rows, 1, o)
+	return multiply(m, subtract(ones, m)) // m * (1 - m)
+}
 
+// Helper functions of easier use of Gonum
 func dot(m, n mat.Matrix) mat.Matrix {
 	r, _ := m.Dims()
 	_, c := n.Dims()
